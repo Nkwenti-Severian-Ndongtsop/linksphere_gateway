8c0542f90e8377867847aa5124fc0b6221070e40
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-07-01 18:28:34 +0100

    Add FrontendgatewayApplication class for Spring Boot application setup

---
+++ frontendgateway/src/main/java/com/linksphere/frontendgateway/frontendgateway/FrontendgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.linksphere.frontendgateway.frontendgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendgatewayApplication.class, args);
+	}
+
+}
