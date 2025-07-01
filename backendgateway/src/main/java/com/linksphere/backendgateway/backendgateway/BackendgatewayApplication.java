905c20dfd92e4735201be81f4dafc283fd256b73
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-07-01 18:23:35 +0100

    Add BackendgatewayApplication class for Spring Boot application entry point

---
+++ backendgateway/src/main/java/com/linksphere/backendgateway/backendgateway/BackendgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.linksphere.backendgateway.backendgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendgatewayApplication.class, args);
+	}
+
+}
