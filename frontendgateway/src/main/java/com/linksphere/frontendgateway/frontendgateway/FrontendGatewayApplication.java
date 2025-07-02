fe66a6a079e643a3850b9e39ac2061fe29c278f8
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-07-02 10:21:29 +0100

    feat: add FrontendGatewayApplication as the main entry point for the frontend gateway

---
+++ frontendgateway/src/main/java/com/linksphere/frontendgateway/frontendgateway/FrontendGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.linksphere.frontendgateway.frontendgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendGatewayApplication.class, args);
+	}
+
+}
